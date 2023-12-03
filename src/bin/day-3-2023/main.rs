8699710d4f5eb29f7af1e8367ff4f7efef20991f
EelisHz <holmsteneelis@gmail.com>
2023-12-04 01:54:37 +0200

    part 2

--- src/bin/day-3-2023/main.rs
+++ src/bin/day-3-2023/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, time::Instant};
 
 #[derive(Debug)]
 struct Number {
@@ -11,11 +11,15 @@ fn is_digit_ascii(c: char) -> bool {
 }
 
 fn is_symbol(c: char) -> bool {
-    // Not digit and not ascii 46 (.)
-    (!is_digit_ascii(c)) && (c as u8 != 46)
+    // Not digit and not dot (.)
+    (!is_digit_ascii(c)) && (c != '.')
 }
 
-fn parse_input(input: &String) -> (Vec<(usize, usize)>, Vec<Vec<Option<u32>>>) {
+fn is_gear(c: char) -> bool {
+    c  == '*'
+}
+
+fn parse_input(input: &String, predicate: fn(char) -> bool) -> (Vec<(usize, usize)>, Vec<Vec<Option<u32>>>) {
     let mut symbols = vec![];
     let mut number_matrix = vec![];
 
@@ -23,7 +27,7 @@ fn parse_input(input: &String) -> (Vec<(usize, usize)>, Vec<Vec<Option<u32>>>) {
         let mut number_row = vec![];
 
         for (j, c) in line.chars().enumerate() {
-            if is_symbol(c) {
+            if predicate(c) {
                 symbols.push((i, j));
             }
             if is_digit_ascii(c) {
@@ -65,8 +69,8 @@ fn print_numbers(numbers_matrix: &Vec<Vec<Option<usize>>>, numbers: &Vec<Number>
     }
 }
 
-fn solve_pt_1(input: &String) -> u32 {
-    let (symbols, digit_matrix) = parse_input(input);
+fn middle_part(digit_matrix: Vec<Vec<Option<u32>>>) -> (Vec<Number>, Vec<Vec<Option<usize>>>) {
+    // Not to be confused with the middle-out algorithm
 
     let x_len = digit_matrix[0].len();
     let y_len = digit_matrix.len();
@@ -101,6 +105,59 @@ fn solve_pt_1(input: &String) -> u32 {
             }
         }
     }
+
+    return (numbers, number_matrix);
+}
+
+fn solve_pt_2(input: &String) -> u32 {
+    let predicate = is_gear;
+    let (symbols, digit_matrix) = parse_input(input, predicate);
+    
+    let x_len = digit_matrix[0].len();
+    let y_len = digit_matrix.len();
+
+    let (mut numbers, number_matrix) = middle_part(digit_matrix);
+
+    let mut sum = 0;
+
+    for (i, j) in symbols {
+        let (i, j) = (i as i32, j as i32);
+        
+        let mut number_a = 0;
+        let mut number_b = 0;
+
+        for x in -1 + i..2 + i {
+            for y in -1 + j..2 + j {
+                let x = x.clamp(0, x_len as i32 - 1) as usize;
+                let y = y.clamp(0, y_len as i32 - 1) as usize;
+                if let Some(number_index) = number_matrix[x][y] {
+                    let number = &mut numbers[number_index];
+                    if !number.counted {
+                        if number_a == 0 {
+                            number_a = number.value;
+                        } else {
+                            number_b = number.value;
+                        }
+                        number.counted = true;
+                    }
+                }
+            }
+        }
+
+        sum += number_a * number_b;
+    }
+
+    return sum;
+}
+
+fn solve_pt_1(input: &String) -> u32 {
+    let predicate = is_symbol;
+    let (symbols, digit_matrix) = parse_input(input, predicate);
+    
+    let x_len = digit_matrix[0].len();
+    let y_len = digit_matrix.len();
+
+    let (mut numbers, number_matrix) = middle_part(digit_matrix);
 
     // print_numbers(&number_matrix, &numbers);
 
@@ -141,9 +198,30 @@ fn main() {
 .664.598.."
         .to_string();
 
+    let test_2 = "467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..".to_string();
+
     assert_eq!(solve_pt_1(&test_1), 4361);
+    assert_eq!(solve_pt_2(&test_2), 467835);
+
+    let timer = Instant::now();
 
     let part_1 = solve_pt_1(&input);
+    let part_2 = solve_pt_2(&input);
 
+    println!("{:?}", timer.elapsed());
+
+    assert_eq!(part_1, 543867);
+    assert_eq!(part_2, 79613331);
+    
     println!("{part_1}");
+    println!("{part_2}");
 }
